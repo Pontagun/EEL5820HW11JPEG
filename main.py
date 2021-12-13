45f9234eec84e55a64e0f516bcba0c7fefb2c46a
Pontakorn Sonchan <pontagun@gmail.com>
2021-12-12 21:33:58 -0500

    So tired

--- main.py
+++ main.py
@@ -5,6 +5,17 @@ from PIL import Image
 import csv
 import json
 
+import zigzig as zz
+
+test_matrix = [[-35, 0, -1, 0, 1, 0, 0, 0],
+               [0, -2, 0, 0, 0, 0, 0, 0],
+               [0, 4, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0]]
+
 q_matrix = [[16, 11, 10, 16, 24, 40, 51, 61],
             [12, 12, 14, 19, 26, 58, 60, 55],
             [14, 13, 16, 24, 40, 57, 69, 56],
@@ -12,8 +23,7 @@ q_matrix = [[16, 11, 10, 16, 24, 40, 51, 61],
             [18, 22, 37, 56, 68, 109, 103, 77],
             [24, 35, 55, 64, 81, 104, 113, 92],
             [49, 64, 78, 87, 103, 121, 120, 101],
-            [72, 92, 95, 98, 112, 100, 103, 99]
-            ]
+            [72, 92, 95, 98, 112, 100, 103, 99]]
 
 
 def dct(input_img, rows, cols):
@@ -30,31 +40,37 @@ def dct(input_img, rows, cols):
                 output_img[u][v] = output_img[u][v] / rows
             else:
                 output_img[u][v] = (output_img[u][v] * 2) / rows
-    return output_img
 
-
-def json_reader(path):
-    f = open(path)
-    data = json.load(f)
-    f.close()
-
-    return data
+    return output_img.real
 
 
 if __name__ == '__main__':
+
+    prev_dc = 0
     path = r'b16.jpg'
-
-    ac_code = json_reader('AC_Code.json')
-    dc_code = json_reader('DC_Code.json')
-    jpg_coef = json_reader('JPEG_Coef.json')
-
     img = cv2.imread(path, 0)
+    dct_img = dct(img, img.shape[0], img.shape[1])
+    img = dct_img
     q_matrix = np.array(q_matrix).astype(float)
 
-    dct_img = dct(img, img.shape[0], img.shape[1])
+    # Uncomment code below to compare with in-class example
+    # prev_dc = -8
+    # img = np.array(test_matrix)
+    # q_matrix = 1
 
-    im = Image.fromarray(abs(dct_img))
+    jpg = ""
+    for i in range(0, img.shape[0], 8):
+        for j in range(0, img.shape[1], 8):
+            img[i:i + 8, j:j + 8] = np.around(img[i:i + 8, j:j + 8] / q_matrix)
+            sq = np.asarray(zz.zigzag(img[i:i + 8, j:j + 8]))
+            sq = np.array(sq).astype(int)
+            jpg_code, prev_dc = zz.code_gen(sq, prev_dc)
+            # jpg.append(jpg_code)
+            jpg = jpg + jpg_code
 
-    if im.mode != 'RGB':
-        im = im.convert('RGB')
-    im.save("64x64dft.jpg")
+    print((prev_dc))
+
+    # im = Image.fromarray(abs(img))
+    # if im.mode != 'RGB':
+    #     im = im.convert('RGB')
+    # im.save("64x64dft.jpg")
---
+++ zigzig.py
@@ -0,0 +1,162 @@
+import json
+
+
+def json_reader(path):
+    f = open(path)
+    data = json.load(f)
+    f.close()
+
+    return data
+
+
+def hex_to_dec(l):
+    if l == 'A':
+        return 10
+    elif l == 'B':
+        return 11
+    elif l == "C":
+        return 12
+    elif l == "D":
+        return 13
+    elif l == "E":
+        return 14
+    elif l == "F":
+        return 15
+    else:
+        return l
+
+def hex_to_dec(l):
+    if l == 10:
+        return 'A'
+    elif l == 11:
+        return 'B'
+    elif l == 12:
+        return 'C'
+    elif l == 13:
+        return 'D'
+    elif l == 14:
+        return 'E'
+    elif l == 15:
+        return 'F'
+    else:
+        return l
+
+
+def bit_revert(s):
+    neg_num = ""
+    for i, val in enumerate(s):
+        if i > 0:
+            neg_num = neg_num + str(1 - int(val))
+
+    return str(neg_num)
+
+
+def get_accode(cat_ac, z_r, val):
+    ac_code = json_reader('AC_Code.json')
+
+    cat_found = False
+    i = 0
+    run_cat = str(hex_to_dec(z_r)) + '/' + str(hex_to_dec(cat_ac))
+    while not cat_found:
+        if ac_code[i]["runcat"] == run_cat:
+            base_code = ac_code[i]["base"]
+            code_len = ac_code[i]["len"]
+            cat_found = True
+        else:
+            if i > len(ac_code):
+                cat_found = True
+
+        i = i + 1
+
+    data_len = str(code_len - len(str(base_code)))
+
+    data = str('{0:0' + data_len + 'b}').format(val)
+    if val < 0:
+        data = bit_revert(data)
+
+    code = base_code + data
+
+    return code
+
+
+def get_dccode(cat, val):
+    cat = int(hex_to_dec(cat))
+    dc_code = json_reader('DC_Code.json')
+    base_code = dc_code[cat]["base"]
+    code_len = dc_code[cat]["len"]
+
+    data_len = str(code_len - len(str(base_code)))
+
+    data = str('{0:0' + data_len + 'b}').format(val)
+    if val < 0:
+        data = bit_revert(data)
+
+    code = base_code + data
+
+    return code
+
+
+def get_jpg_coef(val, t, z_r=0):
+    code = ""
+    jpg_coef = json_reader('JPEG_Coef.json')
+
+    for i, rank in enumerate(jpg_coef):
+
+        if rank["lower"] <= abs(val) <= rank["upper"]:
+            if t == "dc":
+                cat_dc = rank["dccat"]
+                code = get_dccode(cat_dc, val)
+            else:
+                cat_ac = rank["accat"]
+                code = get_accode(cat_ac, z_r, val)
+
+    return code
+
+
+def code_gen(sq, prev_dc):
+    code = ""
+    zero_run = 0
+    for i in range(len(sq)):
+        if i == 0:
+            sq[i] = sq[i] - prev_dc
+            prev_dc = sq[i]
+            # Do DC
+            code_dc = get_jpg_coef(sq[i], "dc")
+            code = code + code_dc
+        else:
+            if sq[i] == 0 or sq[i] == '0':
+                zero_run = zero_run + 1
+            else:
+                code_ac = get_jpg_coef(sq[i], "ac", zero_run)
+                code = code + code_ac
+                zero_run = 0
+
+    code = code + "1010"
+    return code, prev_dc
+
+
+
+
+def zigzag(sq):
+    rows = sq.shape[0]
+    columns = sq.shape[1]
+    solution = [[] for i in range(rows + columns - 1)]
+
+    for r in range(rows):
+        for c in range(columns):
+            sm = r + c
+            if sm % 2 == 0:
+                solution[sm].insert(0, sq[r][c])
+            else:
+                solution[sm].append(sq[r][c])
+
+    return sequence_matrix(solution)
+
+
+def sequence_matrix(s):
+    seq = []
+    for i in s:
+        for j in i:
+            seq.append(j)
+
+    return seq
